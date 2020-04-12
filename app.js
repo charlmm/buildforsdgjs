57dae4e70617f9a65f7329bd5f230b828fbcf093
charlmm <charles.mbuthia19@gmail.com>
2020-04-12 15:15:09 +0300

    assessment backend

---
+++ app.js
@@ -0,0 +1,63 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var morgan = require('morgan')
+var fs = require('fs')
+var path = require('path')
+var estimator = require('./src/estimator.js').covid19ImpactEstimator
+var xml = require('xml')
+
+var app = express()
+
+app.use(bodyParser())
+
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+app.use(morgan(':method  :url  :status  :response-time ms', { stream: accessLogStream}))
+
+
+app.get('/api/v1/on-covid-19/:returnType', (req, res) => {
+	var returnType = req.params.returnType
+	var data = {}
+
+	var region = {}
+	region.name = req.body.name
+	region.avgAge = req.body.avgAge
+	region.avgDailyIncomeInUSD = req.body.avgDailyIncomeInUSD
+	region.avgDailyIncomePopulation = req.body.avgDailyIncomePopulation
+
+	data.region = region
+	data.periodType = req.body.periodType
+	data.timeToElapse = req.body.timeToElapse
+	data.reportedCases = req.body.reportedCases
+	data.population = req.body.population
+	data.totalHospitalBeds = req.body.totalHospitalBeds
+
+	if (returnType == 'json') {
+
+		res.json(estimator(data))
+	} else if (returnType == 'xml') {
+		res.type('ContentType','application/xml')
+		res.send(xml(estimator(data)))
+	}
+})
+
+app.get('/api/v1/on-covid-19', (req,res) => {
+	var returnType = req.params.returnType
+	var data = {}
+	
+	var region = {}
+	region.name = req.body.name
+	region.avgAge = req.body.avgAge
+	region.avgDailyIncomeInUSD = req.body.avgDailyIncomeInUSD
+	region.avgDailyIncomePopulation = req.body.avgDailyIncomePopulation
+
+	data.region = region
+	data.periodType = req.body.periodType
+	data.timeToElapse = req.body.timeToElapse
+	data.reportedCases = req.body.reportedCases
+	data.population = req.body.population
+	data.totalHospitalBeds = req.body.totalHospitalBeds
+	
+	res.json(estimator(data))
+})
+
+app.listen('3000', () => console.log("listening on port 3000"))
--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,35 @@
-const covid19ImpactEstimator = (data) => data;
+var impact = {}
+var severeImpact = {}
 
-export default covid19ImpactEstimator;
+exports.covid19ImpactEstimator = (data) => {
+	currentlyInfected = data.reportedCases * 10
+	impact.currentlyInfected = currentlyInfected
+	severeImpact.currentlyInfected = data.reportedCases * 50
+
+	impact.infectionsByRequestedTime = impact.currentlyInfected * 2**Math.trunc(data.timeToElapse/3)
+	severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * 2**Math.trunc(data.timeToElapse/3)
+
+	impact.severeCasesByRequestedTime = severeImpact.infectionsByRequestedTime * 0.15
+	severeImpact.severeCasesByRequestedTime = severeImpact.infectionsByRequestedTime * 0.15
+
+	totalHospitalBeds = data['totalHospitalBeds'] * 0.35
+
+	impact.hospitalBedsByRequestedTime = totalHospitalBeds - impact.severeCasesByRequestedTime
+	severeImpact.hospitalBedsByRequestedTime = totalHospitalBeds - severeImpact.severeCasesByRequestedTime
+
+	impact.casesForICUByRequestedTime = impact.infectionsByRequestedTime * 0.05
+	severeImpact.casesForICUByRequestedTime = severeImpact.infectionsByRequestedTime * 0.05
+
+	impact.casesForVentilatorsByRequestedTime = impact.infectionsByRequestedTime * 0.02
+	severeImpact.casesForVentilatorsByRequestedTime = severeImpact.infectionsByRequestedTime * 0.02
+
+	dollarsInFlight = severeImpact.infectionsByRequestedTime * 0.65 * 1.5 * 30
+
+	var output = {}
+	output.data = data
+	output.impact = impact
+	output.severeImpact = severeImpact
+
+	return output
+};
+
